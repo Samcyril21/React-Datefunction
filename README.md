f9b28da86ab1377f59bf224c71a6da1f70c5f106
Samcyril21 <119171114+Samcyril21@users.noreply.github.com>
2023-02-23 13:06:17 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Datefunction
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fevcev)
---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  let date=new Date();
+
+  let hours=date.getHours();
+
+  let msg=''
+  let msg2=''
+ 
+  if(hours>=4 & hours<11){
+    msg="Good Morning"
+    msg2='Have A Nice Day'
+  }
+  if(hours>=11 & hours<16){
+    msg="Good Afternoon"
+    msg2='Have A Good Noon '
+  }
+  if(hours>=16 & hours<20){
+   msg="Good Evening"
+   msg2='Have A Nice Day'
+  }
+  if(hours>=20 & hours<4){
+    msg="Good Night"
+    msg2='Have A Nice Day'
+  }
+
+  return (
+    <div id='greet'>
+      <h1>Greetings Of The DAY</h1>
+      
+      <h2>Hi, {msg} Buddy!</h2>
+      <h3>Hi, {msg2} Buddy!</h3>
+      </div>
+    );  
+   
+  }
