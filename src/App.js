33d3c97ed30ad8a06d69ecf6c423863782366dd7
Samcyril21 <119171114+Samcyril21@users.noreply.github.com>
2023-03-13 14:19:11 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 export default function App() {
-  let date=new Date();
+  let date = new Date();
 
-  let hours=date.getHours();
+  let hours = date.getHours();
 
-  let msg=''
-  let msg2=''
- 
-  if(hours>=4 & hours<11){
-    msg="Good Morning"
-    msg2='Have A Nice Day'
+  let msg = '';
+  let msg2 = '';
+
+  if ((hours >= 4) & (hours < 11)) {
+    msg = 'Good Morning';
+    msg2 = 'Have A Nice Day';
   }
-  if(hours>=11 & hours<16){
-    msg="Good Afternoon"
-    msg2='Have A Good Noon '
+  if ((hours >= 11) & (hours < 16)) {
+    msg = 'Good Afternoon';
+    msg2 = 'Have A Good Noon ';
   }
-  if(hours>=16 & hours<20){
-   msg="Good Evening"
-   msg2='Have A Nice Day'
+  if ((hours >= 16) & (hours < 20)) {
+    msg = 'Good Evening';
+    msg2 = 'Have A Nice Day';
   }
-  if(hours>=20 & hours<4){
-    msg="Good Night"
-    msg2='Have A Nice Day'
+  if ((hours >= 20) & (hours < 4)) {
+    msg = 'Good Night';
+    msg2 = 'Have A Nice Day';
   }
 
   return (
-    <div id='greet'>
+    <div id="greet">
       <h1>Greetings Of The DAY</h1>
-      
+
       <h2>Hi, {msg} Buddy!</h2>
-      <h3>Hi, {msg2} Buddy!</h3>
-      </div>
-    );  
-   
-  }
+      <h3> {msg2} Buddy!</h3>
+    </div>
+  );
+}
